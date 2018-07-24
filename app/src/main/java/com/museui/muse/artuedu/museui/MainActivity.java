e4f35c81692a871458601d56cc641970aff63b2e
Arturo Gómez <artuedu@gmail.com>
2018-07-24 09:15:23 -0500

    24/07/2018

--- app/src/main/java/com/museui/muse/artuedu/museui/MainActivity.java
+++ app/src/main/java/com/museui/muse/artuedu/museui/MainActivity.java
@@ -1,5 +1,6 @@
 package com.museui.muse.artuedu.museui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 import com.choosemuse.libmuse.MuseManagerAndroid;
 
@@ -33,14 +35,17 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button btn_comenzar = (Button) findViewById(R.id.btn_comenzar);
+
+        btn_comenzar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent museIntent = new Intent(MainActivity.this, MuseActivity.class);
+
+                startActivity(museIntent);
             }
         });
+
     }
 
     @Override
---
+++ app/src/main/java/com/museui/muse/artuedu/museui/MuseActivity.java
@@ -0,0 +1,13 @@
+package com.museui.muse.artuedu.museui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MuseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_muse);
+    }
+}
